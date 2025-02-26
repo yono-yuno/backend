4850b982e26610c747c01a0452e0446a4de43ec2
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-02-26 23:28:32 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/com/yono_yuno/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yono_yuno.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
