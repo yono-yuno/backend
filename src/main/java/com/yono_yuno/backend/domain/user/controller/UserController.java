e67a0709f3518e14e7167e014d0ec74085a0ba6c
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-02-28 15:10:43 +0900

    ✨ User부분 Swagger Tag 추가

--- src/main/java/com/yono_yuno/backend/domain/user/controller/UserController.java
+++ src/main/java/com/yono_yuno/backend/domain/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.yono_yuno.backend.domain.user.controller;
 
 import com.yono_yuno.backend.domain.user.entity.dto.*;
 import com.yono_yuno.backend.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "회원가입", description = "유저의 정보를 받아서 회원가입을 진행합니다.")
     @PostMapping("/signup")
     public ResponseEntity<Map<String, Object>> signUp(@RequestBody RequestSignUpDTO requestSignUpDTO) {
         boolean success = userService.signUp(requestSignUpDTO);
@@ -33,6 +35,7 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
     }
 
+    @Operation(summary = "로그인", description = "휴대폰번호와 비밀번호로 로그인을 진행합니다.")
     @PostMapping("/login")
     public ResponseEntity<Map<String, Object>> login(@RequestBody RequestLoginDTO requestLoginDTO) {
         ResponseLoginDTO responseLoginDTO = userService.login(requestLoginDTO);
@@ -47,6 +50,7 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
     }
 
+    @Operation(summary = "설정 변경", description = "유저의 상한액, 전체 고민시간 설정을 변경합니다.")
     @PutMapping()
     public ResponseEntity<Map<String, Object>> updateSetting(@RequestBody RequestUpdateSettingDTO requestUpdateSettingDTO) {
         boolean success = userService.updateSetting(requestUpdateSettingDTO);
@@ -58,6 +62,7 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
     }
 
+    @Operation(summary = "유저 조회", description = "userId로 해당 유저의 정보를 조회합니다.")
     @GetMapping()
     public ResponseEntity<Map<String, Object>> getUser(@RequestParam("userId")UUID userId) {
         ResponseGetUserDTO responseGetUserDTO = userService.getUser(userId);
