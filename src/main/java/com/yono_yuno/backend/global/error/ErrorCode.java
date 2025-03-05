67888614e3e602d1536fcba8380712b99a8c9a19
parkstar12 <zzanggu12280@naver.com>
2025-03-05 10:07:33 +0900

    🎨 DIARY_NOT_FOUNT -> DIARY_NOT_FOUND 오타수정

--- src/main/java/com/yono_yuno/backend/global/error/ErrorCode.java
+++ src/main/java/com/yono_yuno/backend/global/error/ErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    DIARY_NOT_FOUNT(HttpStatus.NOT_FOUND, false, "DIARY-001", "소비 일기를 찾을 수 없습니다."),
+    DIARY_NOT_FOUND(HttpStatus.NOT_FOUND, false, "DIARY-001", "소비 일기를 찾을 수 없습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, false, "USER-001", "사용자를 찾을 수 없습니다."),
     USER_IS_EXIST(HttpStatus.BAD_REQUEST, false, "USER-002", "이미 유저가 존재합니다."),
     ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND, false, "ACCOUNT-001", "계좌를 찾을 수 없습니다."),
