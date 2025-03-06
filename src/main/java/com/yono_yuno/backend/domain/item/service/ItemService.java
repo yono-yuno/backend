8f6410d199ccb49f4c748fb824c479adfd78faae
ruka-sh <ori8591y@gmail.com>
2025-03-06 17:40:58 +0900

    ✨ 상품 정렬 기능 추가
    [FEATURE] 상품 조회 시, 상품을 기준에 따라 정렬할 수 있는 기능 추가

--- src/main/java/com/yono_yuno/backend/domain/item/service/ItemService.java
+++ src/main/java/com/yono_yuno/backend/domain/item/service/ItemService.java
@@ -5,8 +5,10 @@ import com.yono_yuno.backend.domain.item.entity.dto.ResponseGetItemAllDTO;
 import com.yono_yuno.backend.domain.item.entity.dto.ResponseGetItemDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class ItemService {
@@ -21,7 +23,33 @@ public class ItemService {
         return getItemBean.exec(itemId);
     }
 
-    public List<ResponseGetItemAllDTO> getItemsByCategory(String category) {
-        return getItemBean.exec(category);
+    public List<ResponseGetItemAllDTO> getItemsByCategory(String category, String sort) {
+        List<ResponseGetItemAllDTO> items = getItemBean.exec(category);
+
+        if(sort != null) {
+            switch (sort) {
+                case "latest":
+                    return items.stream()
+                            .sorted(Comparator.comparing(
+                                    ResponseGetItemAllDTO::getCreateAt,
+                                    Comparator.nullsLast(Comparator.naturalOrder()) // null 값을 마지막으로 정렬
+                            ).reversed())
+//                            .sorted(Comparator.comparing(ResponseGetItemAllDTO::getCreateAt).reversed())
+                            .collect(Collectors.toList());
+                case "oldest":
+                    return items.stream()
+                            .sorted(Comparator.comparing(ResponseGetItemAllDTO::getCreateAt))
+                            .collect(Collectors.toList());
+                case "highPrice":
+                    return items.stream()
+                            .sorted(Comparator.comparing(ResponseGetItemAllDTO::getPrice).reversed())
+                            .collect(Collectors.toList());
+                case "lowPrice":
+                    return items.stream()
+                            .sorted(Comparator.comparing(ResponseGetItemAllDTO::getPrice))
+                            .collect(Collectors.toList());
+            }
+        }
+        return items;
     }
 }
