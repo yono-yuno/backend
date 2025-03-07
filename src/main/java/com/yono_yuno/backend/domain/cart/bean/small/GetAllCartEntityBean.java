b32c1321c891bb5e2e7c983157ca63ffc51f26d2
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-03-07 16:49:24 +0900

    Revert "✨ Alarm 기능 구현"
    This reverts commit 29b797238fefd1c0c9e18a36e2a8f34d18b6a629.

--- src/main/java/com/yono_yuno/backend/domain/cart/bean/small/GetAllCartEntityBean.java
+++ src/main/java/com/yono_yuno/backend/domain/cart/bean/small/GetAllCartEntityBean.java
@@ -1,27 +1,19 @@
 package com.yono_yuno.backend.domain.cart.bean.small;
 
-import com.yono_yuno.backend.domain.cart.entity.AskCount;
 import com.yono_yuno.backend.domain.cart.entity.CartEntity;
 import com.yono_yuno.backend.domain.cart.repository.CartRepositoryJPA;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
-
 @Component
 public class GetAllCartEntityBean {
     private final CartRepositoryJPA cartRepositoryJpa;
 
-    public GetAllCartEntityBean(CartRepositoryJPA cartRepositoryJpa) {
-        this.cartRepositoryJpa = cartRepositoryJpa;
+    public GetAllCartEntityBean (CartRepositoryJPA cartRepositoryJpa){
+        this.cartRepositoryJpa=cartRepositoryJpa;
     }
-
-    public List<CartEntity> exec(UUID userId){
+    public List<CartEntity>exec(UUID userId){
         return cartRepositoryJpa.findAllByUserId(userId);
-    }
-
-    public List<CartEntity> exec(UUID userId, AskCount askCount) {
-        return cartRepositoryJpa.findAllByUserIdAndAskCount(userId, askCount);
     }
 }
--- src/main/java/com/yono_yuno/backend/domain/cart/repository/CartRepositoryJPA.java
+++ src/main/java/com/yono_yuno/backend/domain/cart/repository/CartRepositoryJPA.java
@@ -1,18 +1,11 @@
 package com.yono_yuno.backend.domain.cart.repository;
 
-import com.yono_yuno.backend.domain.cart.entity.AskCount;
 import com.yono_yuno.backend.domain.cart.entity.CartEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface CartRepositoryJPA extends JpaRepository<CartEntity, UUID>{
     List<CartEntity> findAllByUserId(UUID userId);
-
-    List<CartEntity> findAllByUserIdAndAskCount(UUID userId, AskCount askCount);
 }
